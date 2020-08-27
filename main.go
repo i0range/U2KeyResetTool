449bd37880d17bfe2da7a51e060028a468775bf3
Orange <70310304+i0range@users.noreply.github.com>
2020-08-27 15:46:18 +0800

    Reformat code

--- main.go
+++ main.go
@@ -30,46 +30,6 @@ var (
 	httpClient         = &http.Client{}
 )
 
-type Config struct {
-	Host   string
-	Port   uint16
-	User   string
-	Pass   string
-	ApiKey string
-	Proxy  string
-}
-
-func (c Config) validate() bool {
-	if c.Host == "" {
-		return false
-	}
-	if c.Port == 0 {
-		return false
-	}
-	if c.ApiKey == "" {
-		return false
-	}
-	return true
-}
-
-type U2Request struct {
-	JsonRpc string   `json:"jsonrpc"`
-	Method  string   `json:"method"`
-	Params  []string `json:"params"`
-	Id      int      `json:"id"`
-}
-
-type U2Error struct {
-	Code    int    `json:"code,omitempty"`
-	Message string `json:"message,omitempty"`
-}
-
-type U2Response struct {
-	Id     int     `json:"id,omitempty"`
-	Result string  `json:"result,omitempty"`
-	Error  U2Error `json:"error,omitempty"`
-}
-
 func initClient() {
 	commandConfig := parseFlag()
 	if commandConfig != nil {
---
+++ model.go
@@ -0,0 +1,41 @@
+package main
+
+type U2Request struct {
+	JsonRpc string   `json:"jsonrpc"`
+	Method  string   `json:"method"`
+	Params  []string `json:"params"`
+	Id      int      `json:"id"`
+}
+
+type U2Error struct {
+	Code    int    `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+type U2Response struct {
+	Id     int     `json:"id,omitempty"`
+	Result string  `json:"result,omitempty"`
+	Error  U2Error `json:"error,omitempty"`
+}
+
+type Config struct {
+	Host   string
+	Port   uint16
+	User   string
+	Pass   string
+	ApiKey string
+	Proxy  string
+}
+
+func (c Config) validate() bool {
+	if c.Host == "" {
+		return false
+	}
+	if c.Port == 0 {
+		return false
+	}
+	if c.ApiKey == "" {
+		return false
+	}
+	return true
+}
